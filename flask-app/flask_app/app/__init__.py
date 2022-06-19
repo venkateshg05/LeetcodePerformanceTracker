a512cb503b8848e547b651f37d7ca35371288b7e
venkateshg05 <venkatesh_gopal@outlook.com>
2022-06-18 19:46:14 -0500

    (fix) remove db.create_all()

--- flask-app/flask_app/app/__init__.py
+++ flask-app/flask_app/app/__init__.py
@@ -5,8 +5,6 @@ from . import config
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.app_context().push()
 db.init_app(app)
-db.create_all()
 
 from . import routes
