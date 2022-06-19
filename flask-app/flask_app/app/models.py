5f5ebe7fbb62b11ef174a2c8b267e4c7e972894f
venkateshg05 <venkatesh_gopal@outlook.com>
2022-06-18 19:46:36 -0500

    (clean) remove User model

--- flask-app/flask_app/app/models.py
+++ flask-app/flask_app/app/models.py
@@ -3,11 +3,6 @@ import flask_sqlalchemy
 db = flask_sqlalchemy.SQLAlchemy()
 
 
-class Users(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-
-
 class Questions(db.Model):
     __tablename__ = "questions"
     id = db.Column(db.Integer, primary_key=True)
