5d9b60267c4715e72452af188e28cffc260d68d5
venkateshg05 <venkatesh_gopal@outlook.com>
2022-06-20 23:56:53 -0500

    add support for migrations & migrate to new schema

--- flask-app/flask_app/app/__init__.py
+++ flask-app/flask_app/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from .models import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from . import config
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy()
 db.init_app(app)
+migrate = Migrate(app, db)
 
-from . import routes
+from . import routes, models
--- flask-app/flask_app/app/models.py
+++ flask-app/flask_app/app/models.py
@@ -1,12 +1,11 @@
-import flask_sqlalchemy
-
-db = flask_sqlalchemy.SQLAlchemy()
+from enum import unique
+from . import db
 
 
 class Questions(db.Model):
     __tablename__ = "questions"
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(500), nullable=False)
+    url = db.Column(db.String(500), nullable=False, unique=True)
     difficulty_tag = db.Column(db.String(100))
 
     def __repr__(self) -> str:
@@ -19,7 +18,7 @@ class Users(db.Model):
     user_id = db.Column(db.String(500), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey("questions.id"), nullable=False)
     submission_dt = db.Column(db.DateTime(timezone=True), nullable=False)
-    submission_status = db.Column(db.Enum(["Accepted", "Failed"]))
+    submission_status = db.Column(db.Enum("Accepted", "Failed", name="status"))
 
     def __repr__(self) -> str:
         return f"{self.user_id}, {self.question_id}, {self.submission_dt}"
